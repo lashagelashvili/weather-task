187a5d8daa1298c6b3f063a9ac439922de74858e
lashagelashvili <las.gelashvili@gmail.com>
2023-03-11 13:55:29 +0400

    add weather route

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './components/weather/weather.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'weather', pathMatch: 'full' },
+  { path: '**', redirectTo: 'weather' },
+  { path: 'weather', component: WeatherComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
