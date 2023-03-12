bc4d50912c4febdf951339146d46a71c14c9e072
lashagelashvili <las.gelashvili@gmail.com>
2023-03-12 17:18:56 +0400

    add service for weather request

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { WeatherData } from '../models/weather.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  constructor(private readonly http: HttpClient) {}
+
+  getWeather(city: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.weatherApiUrl, {
+      params: new HttpParams()
+        .set('q', city)
+        .set('appid', environment.weatherApiKey),
+    });
+  }
+}
