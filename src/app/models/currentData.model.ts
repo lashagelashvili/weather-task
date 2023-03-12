3ffd6ef868a1a0147c038226722c9872ff4daa5e
lashagelashvili <las.gelashvili@gmail.com>
2023-03-12 17:20:43 +0400

    create current data model

---
+++ src/app/models/currentData.model.ts
@@ -0,0 +1,11 @@
+export interface CurrentData {
+  date: string;
+  weekDay: string;
+  month: string;
+  year: string;
+  icon: string;
+  temperature: number;
+  weather: string;
+  city: string;
+  country: string;
+}
