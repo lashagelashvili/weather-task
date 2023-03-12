09adfc8b9f8899a3f677a3433bb841302102a058
lashagelashvili <las.gelashvili@gmail.com>
2023-03-12 17:19:15 +0400

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WeatherComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, WeatherComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
